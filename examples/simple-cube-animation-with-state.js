9389812a967b5e12433ec77899ad8ca79ff45744
arpu <arpu@users.noreply.github.com>
2024-08-07 19:33:22 +0200

    Update simple-cube-animation-with-state.js

--- examples/simple-cube-animation-with-state.js
+++ examples/simple-cube-animation-with-state.js
@@ -2,51 +2,46 @@
  * Spawns a simple cube with basic animation and state management
  */
 class SimpleCubeAnimationWithState extends CapElement {
-  schema = {};
-  init() {
-    this.state = {
-      count: 0
-    };
-  }
+    @property()
+    count: number = 0
+    @query('#box')
+    box
 
-  increment = () => {
-    this.state.count = this.state.count + 1;
+    init() {}
 
-    // set the x coordinate of the box to the current count
-    const box = this.el.querySelector('#box');
-    box.setAttribute('position', { x: this.state.count, y: 1.6, z: 5 });
+    increment = () => {
+        this.count++
 
-    this.requestUpdate();
-  };
+        // set the x coordinate of the box to the current count
+        this.box.setAttribute('position', { x: this.count, y: 1.6, z: 5 })
+    }
 
-  render() {
-    const { count } = this.state;
-
-    return html`
-      <a-circle
-        class="ui"
-        @click="${this.increment}"
-        rotation="0 0 0"
-        position="0 1.5 5"
-        radius="0.2"
-        color="#ff0000"
-      >
-        <a-troika-text value=${count}> </a-troika-text>
-      </a-circle>
-      <a-box
-        id="box"
-        class="interactable"
-        position="3 1.5 5"
-        color="#333"
-        rotation="0 45 0"
-        animation__position="property: position; from: 0 1.6 5; to: 0 1 5; startEvents: click"
-        animation__color="property: color; from: #333; to: #ff0000; startEvents: click"
-        animation__rotation="property: rotation; from: 0 45 0; to: 0 135 0; startEvents: click"
-        animation-toggle="names: animation__position, animation__color, animation__rotation"
-      >
-      </a-box>
-    `;
-  }
+    render() {
+        return html`
+            <a-circle
+                class="ui"
+                @click="${this.increment}"
+                rotation="0 0 0"
+                position="0 1.5 -1"
+                radius="0.2"
+                color="#ff0000"
+            >
+                <cap-uix-text value=${this.count} font-size="0.1"> </cap-uix-text>
+            </a-circle>
+            <a-box
+                id="box"
+                class="interactable"
+                position="3 1.5 -1"
+                color="#333"
+                rotation="0 45 0"
+                animation__position="property: position; from: 0 1.6 5; to: 0 1 5; startEvents: click"
+                animation__color="property: color; from: #333; to: #ff0000; startEvents: click"
+                animation__rotation="property: rotation; from: 0 45 0; to: 0 135 0; startEvents: click"
+                animation-toggle="names: animation__position, animation__color, animation__rotation"
+            >
+            </a-box>
+        `
+    }
 }
 
-export default SimpleCubeAnimationWithState;
+export default SimpleCubeAnimationWithState
